fbfbaba5eb7c81a4885b10c9d2ddddda515ac726
Alexandre Fernandes <mteknight@gmail.com>
2023-01-22 01:31:33 -0300

    Added test cases to validate the product price. Test fails.

--- Ecommerce.Domain.Tests/Product/ProductTestData.cs
+++ Ecommerce.Domain.Tests/Product/ProductTestData.cs
@@ -1,6 +1,7 @@
-﻿using Ecommerce.Common.Extensions;
-using Ecommerce.Common.Tests;
+﻿using AutoFixture;
 
+using Ecommerce.Common.Extensions;
+using Ecommerce.Common.Tests;
 
 namespace Ecommerce.Domain.Tests.Product;
 
@@ -8,10 +9,19 @@ internal sealed record ProductTestData
 {
     public static IEnumerable<object[]> InvalidProductTestData()
     {
+        var fixture = new Fixture();
+
         return TestData
-            .NewSet(new Domain.Product(default, 0)) 
-            .NewSet(new Domain.Product(string.Empty, 0))
-            .NewSet(new Domain.Product(StringExtensions.Whitespace, 0));
-                
+            .NewSet(new Domain.Product(default, fixture.Create<double>()))
+            .NewSet(new Domain.Product(string.Empty, fixture.Create<double>()))
+            .NewSet(new Domain.Product(StringExtensions.Whitespace, fixture.Create<double>()))
+            .NewSet(new Domain.Product(fixture.Create<string>(), double.NaN))
+            .NewSet(new Domain.Product(fixture.Create<string>(), double.PositiveInfinity))
+            .NewSet(new Domain.Product(fixture.Create<string>(), double.NegativeInfinity))
+            .NewSet(new Domain.Product(fixture.Create<string>(), -1))
+            .NewSet(new Domain.Product(fixture.Create<string>(), -43.765))
+            .NewSet(new Domain.Product(fixture.Create<string>(), double.MinValue))
+            ;
+
     }
 }
